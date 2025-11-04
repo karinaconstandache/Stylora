12a1892a9f2c701efbc3b04b067526e6a97554ed
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2025-11-04 19:34:10 +0200

    change git

--- Stylora.Domain/obj/Debug/net9.0/Stylora.Domain.AssemblyInfo.cs
+++ Stylora.Domain/obj/Debug/net9.0/Stylora.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stylora.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d4c9400f49087d80ba0ead4039c76b4b514c34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d9e4676c26baea49c3250b5c5d01301961bc0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stylora.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stylora.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
